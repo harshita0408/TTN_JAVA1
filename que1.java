d9c2e6aeaeb7f01990664e3151ed39d791c324a4
harshita0408 <69676349+harshita0408@users.noreply.github.com>
2022-09-27 14:02:14 +0530

    Add files via upload

---
+++ que1.java
@@ -0,0 +1,22 @@
+package Java3;
+
+import java.util.*;
+public class que1 {
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.print("Enter the f_name:");
+		String f_name= scn.nextLine();
+		System.out.println("f_name  is: "+ f_name);
+		System.out.print("Enter the l_name:");
+		String l_name = scn.nextLine();
+		System.out.println("l_name is: "+ l_name);
+		System.out.print("Enter the age:");
+		int age = scn.nextInt();
+		System.out.print("age is:" +  age);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
